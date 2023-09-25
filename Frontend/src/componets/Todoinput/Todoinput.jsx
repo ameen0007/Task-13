ab2c522e5a693c925f13749782795395bce3627e
Ameen <ameencrews@example.com>
2023-09-26 03:24:18 +0530

    u

--- Frontend/src/componets/Todoinput/Todoinput.jsx
+++ Frontend/src/componets/Todoinput/Todoinput.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import {useEffect,useRef} from 'react'
 import './todoinput.css'
 
 /----------------------------------------------------------------/
 export const Todoinput = ({HandleFetchInput,AddTodo,userInputData}) => {
 
+  const inputref = useRef()
+   
+  useEffect(() => {
+    inputref.current.focus()
+  }, [])
+  
 
   return (
 
     <div className='totdoinput-container'>
 
-        <input type="text" value={userInputData} placeholder='New Todo'onChange={HandleFetchInput}/>
+        <input ref={inputref} type="text" value={userInputData} placeholder='New Todo'onChange={HandleFetchInput}/>
         <button onClick={AddTodo}>ADD TODO</button>
 
     </div>
